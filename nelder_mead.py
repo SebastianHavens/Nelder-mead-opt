d17b1be7f31f7ba064a38476df5cc89083244301
Sebastian Havens <sebastian.havens@outlook.com>
2022-04-20 12:11:59 +0100

    Changed how restarted calculations run. Writes a new files which contain the temperature at which phase transitions occur. When a calculation is restarted, NM will be called for each iteration that like it would previously but won't run if max number of iterations have already been reached, and ns_analyse won't be called and instead will read phase transition temperatures from the file if they've been calculated previously

--- nelder_mead.py
+++ nelder_mead.py
@@ -4,41 +4,49 @@ import numpy as np
 import copy
 import matscipy.calculators.eam.calculator
 import os
-import sys
 import shutil
 import time
-import shelve
 
 
 # Returns absolute value of distance from experimental value
 def find_peak(prefix, target):
-    try:
-        os.system('./ns_analyse ' + str(prefix) + '.energies -M 1 -n 1000 -D 5 > ' + str(prefix) + '.dat')
-    except:
-        print('Error calling ns_analyse')
-        exit()
-    T, Cp = np.loadtxt((str(prefix) + '.dat'), usecols=(0, 4), unpack=True)
-    index_PT = np.where(Cp == np.amax(Cp))
-    index_PT = index_PT[0]
-    # If multiple temperatures have the same value for hear capacity, this will select the lowest temperature of those
-    # with the same heat capacity
-    index_PT = index_PT[0]
-    print(T[index_PT])
-    # Calculate error from experimental value, we take away an amount dependent on the system size effects from out
-    # calculated phase transition temperature..
+    if os.path.exists(str(prefix) + '_pt_temp'):
+        pt_temp = int(np.loadtxt(str(prefix) + '_pt_temp'))
+        return abs(target - (pt_temp + finite_size_offset))
 
-    h_progress1 = open('progress1', 'a')
-    h_progress1.write(str(call_ns_run_counter) + ' ' + str(T[index_PT]) + '\n')
-    h_progress1.close()
+    else:
+        try:
+            os.system('./ns_analyse ' + str(prefix) + '.energies -M 1 -n 1000 -D 5 > ' + str(prefix) + '.dat')
+        except:
+            print('Error calling ns_analyse')
+            exit()
 
-    return abs(target - (int(T[index_PT]) - 252))
+        t, cp = np.loadtxt((str(prefix) + '.dat'), usecols=(0, 4), unpack=True)
+        index_of_phase_transition = np.where(cp == np.amax(cp))
+        # If multiple temperatures have the same value for heat capacity, will select the lowest temperature of those
+        # with the same heat capacity
+        index_of_phase_transition = index_of_phase_transition[0]
+        pt_temp = int(t[index_of_phase_transition])
+
+        h_pt_temp = open(str(prefix) + '_pt_temp', 'a')
+        h_pt_temp.write(str(pt_temp))
+        h_pt_temp.close()
+
+        h_progress1 = open('progress1', 'a')
+        h_progress1.write(str(call_ns_run_counter) + ' ' + str(t[index_of_phase_transition]) + '\n')
+        h_progress1.close()
+
+        return abs(target - (pt_temp + finite_size_offset))
+
 
 # Function to multiply values in the potential with a linear line across the radius.
 # Create line with the same number of value as the potential and multiply against the potential
 # If statement to deal with if parameters are the same resulting in a flat line - Line spacing calculated using
 # difference between parameters so need another method when they're the same.
+
+
 def gen_poten(param):
-    potential: str = matscipy.calculators.eam.io.read_eam('Cu01.eam.alloy', 'eam/alloy')
+    potential = matscipy.calculators.eam.io.read_eam('Cu01.eam.alloy', 'eam/alloy')
 
     graph_1 = potential[2]
     graph_2 = potential[3]
@@ -53,7 +61,7 @@ def gen_poten(param):
         line_2 = np.full(np.size(graph_2), param[2])
     else:
         line_2 = np.arange(param[2], param[3], ((param[3] - param[2]) / np.size(graph_2)))
-        
+
     if param[4] == param[5]:
         line_3 = np.full(np.size(graph_3), param[4])
     else:
@@ -102,36 +110,6 @@ def call_ns_run(param):
     return score
 
 
-def load_variables():
-    cwd = os.getcwd()
-    shelf = shelve.open(str(cwd + '/variables'))
-    for key in shelf:
-        globals()[key] = shelf[key]
-    shelf.close
-
-
-def save_variables():
-    cwd = os.getcwd()
-    filename = str(cwd) + '/variables'
-    shelf = shelve.open(filename, 'n')
-    for key in globals():
-        try:
-            shelf[key] = globals()[key]
-        except:
-            print('ERROR shleving: {0}'.format(key))
-    shelf.close()
-
-
-opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
-if "-r" in opts:
-    print('Restarting NM calculation')
-    restart = True
-else:
-    restart = False
-
-# TEMPORARY
-
-
 # NM parameters
 step = 0.05
 no_improve_thr = 20
@@ -144,99 +122,33 @@ sigma = 0.5
 
 # Starting parameters
 x_start = np.array([1.0, 1.0, 1.0])
-
-# If two pressures used, needs to be sum of both values.
-experimental = 1400
-
-stage = None
-if restart:
-
-    load_variables()
-    restart = True  # This has to be here otherwise when loading variables it will reset to true.
-    print('have my vars loaded?')
-    print(rscore)
-    # reflection
-    if stage == 0:
-        xr = x0 + alpha * (x0 - res[-1][0])
-        rscore = call_ns_run(xr)
-        h_progress = open('progress', 'a')
-        h_progress.write(str(call_ns_run_counter) + ' Reflection ' + str(xr) + ' ' + str(rscore) + '\n')
-        h_progress.close()
-        if res[0][1] <= rscore < res[-2][1]:
-            del res[-1]
-            res.append([xr, rscore])
-        else:
-            stage = 1
-
-    # expansion
-    if stage == 1:
-        if rscore < res[0][1]:
-            xe = x0 + gamma * (x0 - res[-1][0])
-            escore = call_ns_run(xe)
-            h_progress = open('progress', 'a')
-            h_progress.write(str(call_ns_run_counter) + ' Expansion ' + str(xe) + ' ' + str(escore) + '\n')
-            h_progress.close()
-            if escore < rscore:
-                del res[-1]
-                res.append([xe, escore])
-            else:
-                del res[-1]
-                res.append([xr, rscore])
-        else:
-            stage = 2
-
-    # contraction
-    if stage == 2:
-        xc = x0 + rho * (x0 - res[-1][0])
-        cscore = call_ns_run(xc)
-        h_progress = open('progress', 'a')
-        h_progress.write(str(call_ns_run_counter) + ' Contraction ' + str(xc) + ' ' + str(cscore) + '\n')
-        h_progress.close()
-        if cscore < res[-1][1]:
-            del res[-1]
-            res.append([xc, cscore])
-        else:
-            stage = 3
-
-    # reduction
-    if stage == 3:
-        x1 = res[0][0]
-        nres = []
-
-        # for tup in res:
-        for tup in res:  # might be a bug here from the loop type change, check
-            redx = x1 + sigma * (tup[0] - x1)
-            score = call_ns_run(redx)
-            h_progress = open('progress', 'a')
-            h_progress.write(str(call_ns_run_counter) + ' Reduction ' + str(redx) + ' ' + str(score) + '\n')
-            h_progress.close()
-            nres.append([redx, score])
-        res = nres
+finite_size_offset = -252  # Difference between infinite system size and finite system size (infinite - finite)
 
 # Progress file
-if not restart:
-    call_ns_run_counter = 0
-    h_progress = open('progress', 'a')
-    h_progress.write('NS count:  Stage:  Parameters:   Score: \n')
-    h_progress.close()
-    # init
-    dim = len(x_start)
-    prev_best = call_ns_run(x_start)
+
+
+call_ns_run_counter = 0
+h_progress = open('progress', 'a')
+h_progress.write('NS count:  Stage:  Parameters:   Score: \n')
+h_progress.close()
+# init
+dim = len(x_start)
+prev_best = call_ns_run(x_start)
+h_progress = open('progress', 'a')
+h_progress.write(str(call_ns_run_counter) + ' Init ' + str(x_start) + ' ' + str(prev_best) + '\n')
+h_progress.close()
+no_improv = 0
+res = [[x_start, prev_best]]
+iters = 0
+
+for i in range(dim):
+    x = copy.copy(x_start)
+    x[i] = x[i] + step
+    score = call_ns_run(x)
     h_progress = open('progress', 'a')
     h_progress.write(str(call_ns_run_counter) + ' Init ' + str(x) + ' ' + str(score) + '\n')
     h_progress.close()
-    no_improv = 0
-    res = [[x_start, prev_best]]
-    iters = 0
-
-    for i in range(dim):
-        x = copy.copy(x_start)
-        x[i] = x[i] + step
-        score = call_ns_run(x)
-        h_progress = open('progress', 'a')
-        h_progress.write(str(call_ns_run_counter) + ' Init ' + str(x) + ' ' + str(score) + '\n')
-        h_progress.close()
-        res.append([x, score])
+    res.append([x, score])
 
 # simplex iter
 while 1:
@@ -284,8 +196,6 @@ while 1:
             x0[i] += c / (len(res) - 1)
 
     # reflection
-    stage = 0
-    save_variables()
     xr = x0 + alpha * (x0 - res[-1][0])
     rscore = call_ns_run(xr)
     h_progress = open('progress', 'a')
@@ -298,8 +208,6 @@ while 1:
 
     # expansion
     if rscore < res[0][1]:
-        stage = 1
-        save_variables()
         xe = x0 + gamma * (x0 - res[-1][0])
         escore = call_ns_run(xe)
         h_progress = open('progress', 'a')
@@ -315,8 +223,6 @@ while 1:
             continue
 
     # contraction
-    stage = 2
-    save_variables()
     xc = x0 + rho * (x0 - res[-1][0])
     cscore = call_ns_run(xc)
     h_progress = open('progress', 'a')
@@ -328,8 +234,6 @@ while 1:
         continue
 
     # reduction
-    stage = 3
-    save_variables()
     x1 = res[0][0]
     nres = []
 
